fc44d8d7dc60ed693377f2b197abcb83f7db6d10
Maximilian Witte <maximilianwitte@outlook.de>
2020-02-18 17:53:56 +0100

    Init

---
+++ src/router.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Front from './views/front.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Front
+    },
+/*     {
+      path: '/seeds',
+      name: 'seeds',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" *//*  './views/seeds.vue')
+     }, */
+  ]
+})
