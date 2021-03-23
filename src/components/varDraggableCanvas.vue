b9ebacb538a0eb7435b67a7e454873ccf3913dee
Maximilian Witte <maximilianwitte@outlook.de>
2021-03-23 08:02:34 +0100

    pre Try PlainDraggable + LeadLine

--- src/components/varDraggableCanvas.vue
+++ src/components/varDraggableCanvas.vue
@@ -4,11 +4,11 @@
       <div
         v-if="item.id != openID"
         @click.right="setOpen(item.id)"
-        @click.left="buildLine()"
         draggable
         @click.right.prevent
         @dragover.prevent
         @dragenter.prevent
+        @dragend="moveAway(item.id)"
         @drag="onDrag(item.id)"
         class="rect absolute text-2xl text-bg w-64 py-12 rounded-lg cursor-pointer flex justify-center"
         :style="{
@@ -81,51 +81,57 @@ export default {
   },
   methods: {
     onDrag(id) {
-      // Make the DIV element draggable:
-      dragElement(document.getElementById(id));
+      const elmnt = document.getElementById(id);
+      console.log("triggered");
+      var pos1 = 0,
+        pos2 = 0,
+        pos3 = 0,
+        pos4 = 0;
 
-      function dragElement(elmnt) {
-        var pos1 = 0,
-          pos2 = 0,
-          pos3 = 0,
-          pos4 = 0;
-        if (document.getElementById(elmnt.id)) {
-          // if present, the header is where you move the DIV from:
-          document.getElementById(elmnt.id).onmousedown = dragMouseDown;
-        } else {
-          // otherwise, move the DIV from anywhere inside the DIV:
-          elmnt.onmousedown = dragMouseDown;
-        }
+      document.getElementById(elmnt.id).onmousedown = dragMouseDown;
 
-        function dragMouseDown(e) {
-          e = e || window.event;
-          e.preventDefault();
-          // get the mouse cursor position at startup:
-          pos3 = e.clientX;
-          pos4 = e.clientY;
-          document.onmouseup = closeDragElement;
-          // call a function whenever the cursor moves:
-          document.onmousemove = elementDrag;
-        }
+      function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+      }
 
-        function elementDrag(e) {
-          e = e || window.event;
-          e.preventDefault();
-          // calculate the new cursor position:
-          pos1 = pos3 - e.clientX;
-          pos2 = pos4 - e.clientY;
-          pos3 = e.clientX;
-          pos4 = e.clientY;
-          // set the element's new position:
-          elmnt.style.top = elmnt.offsetTop - pos2 + "px";
-          elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
-        }
-
-        function closeDragElement() {
-          // stop moving when mouse button is released:
-          document.onmouseup = null;
-          document.onmousemove = null;
-        }
+      function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+        elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+      }
+      function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+        console.log(elmnt.style.top);
+      }
+    },
+    moveAway(id) {
+      const elmnt = document.getElementById(id);
+      const dim = {
+        top : elmnt.style.top,
+        left : elmnt.style.left
+      }
+      for (var i = 0; i < this.items.length; i++) {
+        var elmnt2 = document.getElementById(this.items[i].id);
+        const dim2 = {
+        top : elmnt2.style.top,
+        left : elmnt2.style.left
+      }
+      
       }
     },
     buildLine() {
@@ -174,7 +180,6 @@ export default {
           angle +
           "deg);' />";
         //
-        alert(htmlLine);
         document.body.innerHTML += htmlLine;
       }
 
