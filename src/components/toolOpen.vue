9d8e98e365176e010b98fdd4cbdba2923aaedc4d
Maximilian Witte <maximilianwitte@outlook.de>
2021-05-26 18:44:15 +0200

    fix tool open bug arrow

--- src/components/toolOpen.vue
+++ src/components/toolOpen.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="toolOpen">
-        <div class="header flex text-xl">
-            <svg @click="setOpen" class="w-6 cursor-pointer" :style="getRotation" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20.5 13"><path d="M0 0h20.5l-10 13L0 0z"/></svg>
+        <div class="header h-6 flex text-xl">
+            <svg @click="setOpen" class="w-6 mt-1 cursor-pointer" :style="getRotation" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20.5 13"><path d="M0 0h20.5l-10 13L0 0z"/></svg>
             <h4 class="ml-4">Instructions</h4>
         </div>
         <div v-if="open" class="data text-lg mt-6 ml-2">
