090d6bcffde7282c1df654ef0969d661047e522c
Maximilian Witte <maximilianwitte@outlook.de>
2021-06-08 10:51:27 +0200

    gtagjs instead of vue-analytics

--- src/main.js
+++ src/main.js
@@ -2,20 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-//import VueAnalytics from 'vue-ua'
 Vue.config.productionTip = false
 
+import gtagjs from 'vue-gtagjs'
+gtagjs(router, "G-DQQD9C21EP", {debug: true, scriptId: 'gtagjs'})
+
 Vue.use(require('vue-chartist'))
-/* Vue.use(VueAnalytics, {
-  // [Required] The name of your app as specified in Google Analytics.
-  appName: 'simplyscenariodb',
-  // [Required] The version of your app.
-  appVersion: '1.0',
-  // [Required] Your Google Analytics tracking ID.
-  trackingId: 'G-DQQD9C21EP',
-  // If you're using vue-router, pass the router instance here.
-  vueRouter: router,
-}) */
 new Vue({
   router,
   store,
